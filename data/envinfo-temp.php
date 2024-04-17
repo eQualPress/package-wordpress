51af75799aa7dea3a776eb9205403852b142c629
AlexisVS <alexis.vansan1440@gmail.com>
2024-04-17 16:12:06 +0200

    update

---
+++ data/envinfo-temp.php
@@ -0,0 +1,27 @@
+<?php
+
+list( $params, $providers ) = eQual::announce( [
+	'description' => 'Returns the installation configuration adapted for eQualPress.',
+	'access'      => [
+		'visibility' => 'public'
+	],
+	'response'    => [
+		'content-type'  => 'application/json',
+		'charset'       => 'UTF-8',
+		'accept-origin' => '*'
+	],
+	'providers'   => [ 'context' ]
+] );
+
+list( $context ) = [ $providers['context'] ];
+
+$config = eQual::run( 'get', 'envinfo-temp' );
+
+//$config = json_decode( $config, true );
+//
+$config['backend_url'] = $config['backend_url'] . '/equal.php';
+
+$context->httpResponse()
+        ->status( 201 )
+        ->body( $config )
+        ->send();
